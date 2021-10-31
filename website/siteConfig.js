404429201c3cb9ef97a38c812f16a6d816166faf
Graysaka <66275184+Graysaka@users.noreply.github.com>
2021-10-31 12:43:56 -0400

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -14,7 +14,7 @@ const users = [];
 const siteConfig = {
     title: 'Grayson A. Kass', // Title for your website.
     tagline: 'Live. Laugh. Code.',
-    profilePhoto: '.docs/assets/PortfolioProfile.jpg',
+    profilePhoto: '.assets/PortfolioProfile.jpg',
     url: 'https://graysaka.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
